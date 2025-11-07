351e72e4cd8ef19a0d86534ebf4c2e7e04c23c33
souhila-kb <110311558+souhila-kb@users.noreply.github.com>
2025-11-07 15:32:50 +0100

    Create triangulator.py
    Création du fichier triangulator.py avec les stubs des fonctions

---
+++ TP/triangulator/triangulator.py
@@ -0,0 +1,11 @@
+class Triangulator:
+    """
+    Classe principale pour la triangulation.
+    La logique n'est pas encore implémentée.
+    """
+
+    def triangulate(self, point_set):
+        """
+        Doit effectuer une triangulation d’un ensemble de points.
+        """
+        raise NotImplementedError("Méthode triangulate non implémentée.")
